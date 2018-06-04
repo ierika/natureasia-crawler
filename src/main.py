43130257dcc219a2f776f2cace9b6d7f99566280
Erika <erika.suzuki@springernature.com>
2018-06-04 19:15:28 +0900

    mmmk accept non-https URLS too

--- src/main.py
+++ src/main.py
@@ -17,32 +17,27 @@ class MySpider(CrawlSpider):
     Excludes 'zh-tw' website though.
     """
     name = 'natureasia.com'
-    allowed_domains = ['natureasia.com', 'www.natureasia.com']
+    allowed_domains = ['natureasia.com']
+    start_urls = [
+        'https://www.natureasia.com/ja-jp',
+        'https://www.natureasia.com/ko-kr',
+        'https://www.natureasia.com/en',
+    ]
     root_url = 'https://www.natureasia.com'
+    languages_regex = r'(ja-jp|en|ko-kr)'
     rules = (
         Rule(
             LinkExtractor(
-                allow=r'^https?://www\.natureasia\.com/(ja-jp|ko-kr|en).*$',
-                deny=[
-                    r'^https?://www\.natureasia\.com/en/(middleeast|nindia)$',
-                    r'^https?://www\.natureasia\.com/secure/.*$',
-                ],
-                allow_domains=allowed_domains,
+                allow=r'natureasia\.com/{}'.format(languages_regex),
+                deny=(
+                    r'natureasia\.com/en/(nmiddleeast|nindia)',
+                    r'natureasia\.com/secure',
+                ),
             ),
             callback='parse_item',
             follow=True,
         ),
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Define start URLs
-        self.start_urls = [
-            self.root_url + '/ja-jp',
-            self.root_url + '/en',
-            self.root_url + '/ko-kr',
-            # self.root_url + '/zh-cn',
-        ]
 
     @property
     def export_file(self):
@@ -63,29 +58,48 @@ class MySpider(CrawlSpider):
         update the priority instead just in case it changed.
         Otherwise, just create an entry.
         """
-        if response.url.startswith('https'):
-            language, priority = self.parse_url(response.url)
-            try:
-                url = models.Url.get(url=response.url)
-                url.priority = priority
-                url.language = language
-                url.update = datetime.now()
-                url.save()
-                print('{} entry updated.'.format(response.url))
-            except Exception as e:
-                models.Url.create(
-                    url=response.url,
-                    priority=priority,
-                    language=language,
-                )
-                print('{} entry created.'.format(response.url))
+        language, priority = self.parse_url(response.url)
+        try:
+            url = models.Url.get(url=response.url)
+            url.priority = priority
+            url.language = language
+            url.update = datetime.now()
+            url.save()
+            print('{} entry updated.'.format(response.url))
+        except Exception as e:
+            models.Url.create(
+                url=response.url,
+                priority=priority,
+                language=language,
+            )
+            print('{} entry created.'.format(response.url))
 
     def parse_url(self, url):
         patterns = [
-            (r'^https?://www.natureasia.com/(ja-jp|zh-tw|en)/?$', 1.0),
-            (r'^https?://www.natureasia.com/(ja-jp|zh-tw|en)/[\w\-\.]+/?$', 1.0),
-            (r'^https?://www.natureasia.com/(ja-jp|zh-tw|en)/[\w\-\.]+/toc/?$', 1.0),
-            (r'^https?://www.natureasia.com/(ja-jp|zh-tw|en)/[\w\-\.]+/.+/?$', 0.7),
+            (
+                r'^https?://www.natureasia.com/{}/?$'.format(
+                    self.languages_regex,
+                ),
+                1.0
+            ),
+            (
+                r'^https?://www.natureasia.com/{}/[\w\-\.]+/?$'.format(
+                    self.languages_regex,
+                ),
+                1.0,
+            ),
+            (
+                r'^https?://www.natureasia.com/{}/[\w\-\.]+/toc/?$'.format(
+                    self.languages_regex,
+                ),
+                1.0,
+            ),
+            (
+                r'^https?://www.natureasia.com/{}/[\w\-\.]+/.+/?$'.format(
+                    self.languages_regex,
+                ),
+                0.7,
+            ),
         ]
         for pattern, priority in patterns:
             match = re.match(pattern, url)
